9bc1f9c44602745a971b3e35c0e19ed4a4765548
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-03-04 12:05:11 +0100

    load data from json

--- Codio/Zootpoia/animals_web_generator.py
+++ Codio/Zootpoia/animals_web_generator.py
@@ -1,9 +1,16 @@
+import json
 
 
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
 
 
 def main():
-    pass
+    animals_data = load_data('animals_data.json')
+    print(animals_data)
 
 if __name__ == "__main__":
     main()
