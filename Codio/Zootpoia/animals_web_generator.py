bd7b31f735f7524fb97f03a9149a0d244a51ff11
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-03-05 11:13:27 +0100

    print info from data

--- Codio/Zootpoia/animals_web_generator.py
+++ Codio/Zootpoia/animals_web_generator.py
@@ -1,16 +1,41 @@
 import json
 
 
-
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def print_info(data):
+    for animal in data:
+        output_info = {}
+
+        characteristics = animal.get("characteristics", {})
+
+        # Check and add each field if it exists
+        if name := animal.get("name"):
+            output_info["Name"] = name
+
+        if diet := characteristics.get("diet"):
+            output_info["Diet"] = diet
+
+        if location := animal.get("locations", [None])[0]:
+            output_info["Location"] = location
+
+        if ani_type := characteristics.get("type"):
+            output_info["Type"] = ani_type
+
+        if output_info:
+            for key, value in output_info.items():
+                print(f"{key}: {value}")
+            print()
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    print(animals_data)
+    print_info(animals_data)
+
 
 if __name__ == "__main__":
     main()
