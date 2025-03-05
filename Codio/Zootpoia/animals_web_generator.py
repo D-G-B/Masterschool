184dd114ac7436af9bbff6c3d0a359b53252da0b
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-03-05 11:35:27 +0100

    add animal data to HTML

--- Codio/Zootpoia/animals_web_generator.py
+++ Codio/Zootpoia/animals_web_generator.py
@@ -7,7 +7,10 @@ def load_data(file_path):
         return json.load(handle)
 
 
-def print_info(data):
+def make_animals_string(data):
+
+    output = ""
+
     for animal in data:
         output_parts = []
 
@@ -24,12 +27,35 @@ def print_info(data):
             output_parts.append(f"Type: {ani_type}")
 
         if output_parts:
-            print("\n".join(output_parts) + "\n")
+            output += "\n".join(output_parts) + "\n\n"
+
+    return output
+
+def read_template(html_path):
+    with open(html_path, "r") as file:
+        file_data = file.read()
+    return file_data
+
+def write_html_file(html_content):
+    with open("animals.html", "w") as file:
+        file.write(html_content)
 
 
 def main():
+    # Load animal data
     animals_data = load_data('animals_data.json')
-    print_info(animals_data)
+
+    # Read the HTML template
+    template = read_template('animals_template.html')
+
+    # Generate the string of animal information
+    animals_string = make_animals_string(animals_data)
+
+    # Replace the placeholder in the template
+    output_html = template.replace('__REPLACE_ANIMALS_INFO__', animals_string)
+
+    # Write the new HTML file
+    write_html_file(output_html)
 
 
 if __name__ == "__main__":
